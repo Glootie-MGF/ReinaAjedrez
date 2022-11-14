296841850b051d5aedc0075b06572dd9b9230956
martafernandez <martafernandez@pc167-13.04004620.04.andared.ced.junta-andalucia.es>
2022-11-14 12:12:41 +0100

    He creado los métodos get y set para los atributos.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -2,4 +2,27 @@ package org.iesalandalus.programacion.reinaajedrez.modelo;
 
 public class Posicion {
 
+	private int fila;
+	private char columna;
+	
+	//Método de acceso y modificación
+	public int getFila() {
+		return fila;
+	}
+	
+	public void setFila(int fila) { 
+		if(fila < 1 || fila > 8) {
+			throw new IllegalArgumentException("No está dentro de los límites");
+		}
+		this.fila = fila;
+	}
+	public int getColumna() {
+		return columna;
+	}
+	public void setColumna(char columna) {
+		if(columna < 'a' || columna > 'h') {
+			throw new IllegalArgumentException("No está dentro de los límites");
+		}
+		this.columna = columna;	
+	}
 }
