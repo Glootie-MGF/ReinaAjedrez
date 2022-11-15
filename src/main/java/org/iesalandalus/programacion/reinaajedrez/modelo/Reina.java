6a356fdb67388d2be3ba6f45c9f9fc72e3926854
martafernandez <martafernandez@pc167-13.04004620.04.andared.ced.junta-andalucia.es>
2022-11-15 14:21:56 +0100

    He creado el método mover

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -1,11 +1,83 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
+import javax.naming.OperationNotSupportedException;
 
 public class Reina {
 
 	private Color color;
 	private Posicion posicion;
 	
+	//Método
+	public void mover(Direccion direccion, int pasos) throws OperationNotSupportedException {
+		if (direccion == null) {
+			throw new IllegalArgumentException("La dirección no puede ser nula.");
+		}
+		if (pasos <= 0 || pasos > 7) {
+			throw new IllegalArgumentException("El número de pasos debe ser mayor que cero y menor que 7.");
+		}
+		String movimientoNoValido = "Movimiento no válido: ";
+		switch (direccion) {
+			case NORTE:
+				try {
+					Posicion posicionNueva = new Posicion(posicion.getFila() + pasos, posicion.getColumna());
+				} catch (IllegalArgumentException e) {
+					throw new OperationNotSupportedException(movimientoNoValido + e.getMessage());
+				}
+				break;
+			case NORESTE:
+				try {
+					Posicion posicionNueva = new Posicion(posicion.getFila() + pasos, (char)(posicion.getColumna() + pasos));
+				} catch (IllegalArgumentException e) {
+					throw new OperationNotSupportedException(movimientoNoValido + e.getMessage());
+				}
+				break;
+			case ESTE:
+				try {
+					Posicion posicionNueva = new Posicion(posicion.getFila(), (char)(posicion.getColumna() + pasos));
+				} catch (IllegalArgumentException e) {
+					throw new OperationNotSupportedException(movimientoNoValido + e.getMessage());
+				}
+				break;
+			case SURESTE:
+				try {
+					Posicion posicionNueva = new Posicion(posicion.getFila() - pasos, (char)(posicion.getColumna() + pasos));
+				} catch (IllegalArgumentException e) {
+					throw new OperationNotSupportedException(movimientoNoValido + e.getMessage());
+				}
+				break;
+			case SUR:
+				try {
+					Posicion posicionNueva = new Posicion(posicion.getFila() - pasos, posicion.getColumna());
+				} catch (IllegalArgumentException e) {
+					throw new OperationNotSupportedException(movimientoNoValido + e.getMessage());
+				}
+				break;
+			case SUROESTE:
+				try {
+					Posicion posicionNueva = new Posicion(posicion.getFila() - pasos, (char)(posicion.getColumna() - pasos));
+				} catch (IllegalArgumentException e) {
+					throw new OperationNotSupportedException(movimientoNoValido + e.getMessage());
+				}
+				break;
+			case OESTE:
+				try {
+					Posicion posicionNueva = new Posicion(posicion.getFila(), (char)(posicion.getColumna() - pasos));
+				} catch (IllegalArgumentException e) {
+					throw new OperationNotSupportedException(movimientoNoValido + e.getMessage());
+				}
+				break;
+			case NOROESTE:
+				try {
+					Posicion posicionNueva = new Posicion(posicion.getFila() + pasos, (char)(posicion.getColumna() - pasos));
+				} catch (IllegalArgumentException e) {
+					throw new OperationNotSupportedException(movimientoNoValido + e.getMessage());
+				}
+				break;
+			default:
+				break;
+			}
+	}
+
 	//Constructor por defecto
 	public Reina() {
 		 
