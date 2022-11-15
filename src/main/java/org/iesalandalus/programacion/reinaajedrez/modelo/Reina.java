80b69fc2817d84f0fd2269bdff1c3ab539de69b0
martafernandez <martafernandez@pc167-13.04004620.04.andared.ced.junta-andalucia.es>
2022-11-15 13:00:21 +0100

    He hecho los métodos get y set para cada atributo de la clase Reina. Los set con las excepciones

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -1,5 +1,33 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
+import EjerciciosIntro.Posicion;
+
 public class Reina {
 
+	private Color color;
+	private Posicion posicion;
+	
+	public Color getColor() {
+		return color;
+	}
+	
+	private void setColor(Color color) {
+		if(color == null) {
+			throw new NullPointerException("No puede ser color nulo");
+		}
+		this.color = color;
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	
+	private void setPosicion(Posicion posicion) {
+		if(posicion == null) {
+			throw new NullPointerException("No puede ser posición nula");
+		}
+		this.posicion = new Posicion(posicion);
+	}
+	
+	
 }
