a8dac3e5801170189884db2aa979b84f56d0aadb
martafernandez <martafernandez@pc167-13.04004620.04.andared.ced.junta-andalucia.es>
2022-11-16 13:48:55 +0100

    He creado el método elegirColor.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Consola.java
@@ -1,5 +1,46 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class Consola {
 
+	private Consola() {
+		
+	}
+	public static void mostrarMenu() {
+		System.out.println("-----MENU-----");
+		System.out.println("1- Crear Reina por defecto.");
+		System.out.println("2- Crear Reina eligiendo color.");
+		System.out.println("3- Mover Reina.");
+		System.out.println("4- Salir.");
+	}
+	public static int elegirOpcionMenu() {
+		int opcion;
+		do {
+			System.out.println("ELIGE UNA OPCIÓN DEL 1 AL 4:");
+			opcion = Entrada.entero();	
+		}while(opcion < 1 || opcion > 4);
+		
+		return opcion;
+	}
+	public static Color elegirColor() {
+		System.out.println("-----ELEGIR COLOR-----");
+		System.out.println("1- Reina Blanca.");
+		System.out.println("2- Reina Negra.");
+		
+		int opcionColor;
+		Color colorElegido = null;
+		do {
+			System.out.println("ELIGE UNA OPCIÓN:");
+			opcionColor = Entrada.entero();	
+		}while(opcionColor < 1 || opcionColor > 2);
+		
+		if (opcionColor==1) {
+			colorElegido = Color.BLANCO;
+		}else if (opcionColor==2) {
+			colorElegido = Color.NEGRO;
+		}
+		
+		return colorElegido;
+	}
 }
